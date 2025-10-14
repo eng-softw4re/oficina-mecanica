2e0b4e2ccb1c284e125c65dd963d38fd7526d669
Riam Stefesom <riamstefeson616@gmail.com>
2025-10-13 22:44:36 -0300

    feat: criando a url para veículos.

--- back-end/api/urls.py
+++ back-end/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import cliente_create, get_cliente, update_cliente, delete_cliente
-
+from .views import (
+    cliente_create, get_cliente, update_cliente, delete_cliente,
+    veiculo_create
+)
 urlpatterns = [
     path("clientes/", cliente_create, name="cliente-create"),
     path("clientes/<int:pk>", get_cliente, name="get-cliente"),
     path("clientes/update/", update_cliente, name="put-cliente"),
-    path("clientes/delete/", delete_cliente, name="delete-cliente")
+    path("clientes/delete/", delete_cliente, name="delete-cliente"),
+
+    path("veiculos/", veiculo_create, name="veiculo-create")
 ]
