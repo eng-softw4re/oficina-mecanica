ae8ac572691415d00213dc01b0cad45c4f367342
Riam Stefesom <riamstefeson616@gmail.com>
2025-10-13 22:44:07 -0300

    feat: criando serializer para Veiculos.

--- back-end/api/serializers.py
+++ back-end/api/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
-from .models import Cliente
+from .models import Cliente, Veiculo
 
 class ClienteSerializer(serializers.ModelSerializer):
   class Meta:
     model = Cliente
     fields = ["id", "nome", "cpf", "telefone", "endereco", "data_nascimento"]
-    
+  
+class VeiculoSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Veiculo
+    fields = ["id", "marca", "modelo", "tipo", "cor", "placa", "cliente"]
