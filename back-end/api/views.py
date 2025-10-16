5643ae787ec6fe565f6cf46eed00951eeaf66f37
ariadny <ariadnyfran15@gmail.com>
2025-10-16 20:18:53 -0300

    feat: add metodo POST de Ordem de Serviço

--- back-end/api/views.py
+++ back-end/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Cliente, Veiculo, Procedimento
-from .serializers import ClienteSerializer, VeiculoSerializer, ProcedimentoSerializer
+from .models import Cliente, Veiculo, Procedimento, OrdemServico
+from .serializers import ClienteSerializer, VeiculoSerializer, ProcedimentoSerializer, OrdemServicoSerializer
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
@@ -144,3 +144,12 @@ def delete_procedimento(request, pk):
   procedimento.delete()
   
   return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['POST'])
+def create_ordem(request):  
+  if request.method == "POST":
+    serializer = OrdemServicoSerializer(data=request.data) 
+    if serializer.is_valid(): # valida os dados
+      serializer.save() # salva os dados no banco
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
