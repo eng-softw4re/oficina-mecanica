9c45ac1291e911716c0532fe8405ac0d8e0d7e2c
ariadny <ariadnyfran15@gmail.com>
2025-10-18 17:27:48 -0300

    feat: adicionando url para o metodo de calcular valor total da ordem de serviço

--- back-end/api/urls.py
+++ back-end/api/urls.py
@@ -3,7 +3,7 @@ from .views import (
     cliente_create, get_cliente, update_cliente, delete_cliente,
     veiculo_create, get_veiculo, update_veiculo, delete_veiculo,
     create_procedimento, get_procedimento, update_procedimento, delete_procedimento,
-    create_ordem, get_ordem, update_ordem, delete_ordem,
+    create_ordem, get_ordem, update_ordem, delete_ordem, get_ordem_total,
     create_insumo, get_insumo, update_insumo, delete_insumo,
     create_insumo_os, get_insumo_os, update_insumo_os, delete_insumo_os,
 )
@@ -27,11 +27,12 @@ urlpatterns = [
     path("insumos/<int:pk>", get_insumo, name="get-insumo"),
     path("insumos/update/<int:pk>", update_insumo, name="update-insumo"),
     path("insumos/delete/<int:pk>", delete_insumo, name="delete-insumo"),
-    
+
     path("ordemServicos/", create_ordem, name="create-ordem"),
     path("ordemServicos/<int:pk>", get_ordem, name="get-ordem"),
     path("ordemServicos/update/<int:pk>", update_ordem, name="update-ordem"),
     path("ordemServicos/delete/<int:pk>", delete_ordem, name="delete-ordem"),
+    path("ordemServicos/calcularValorTotal/<int:pk>", get_ordem_total, name="get-ordem-total"),
 
     path("insumosOrdem/", create_insumo_os, name="create-insumoOrdem,"),
     path("insumosOrdem/<int:pk>", get_insumo_os, name="get-insumoOrdem"),
