19f5388c8bcdd7f04760ff4b7d7a093832f49c21
ariadny <ariadnyfran15@gmail.com>
2025-10-17 22:06:29 -0300

    feat: adicionando a tabela de insumos no models

--- back-end/api/models.py
+++ back-end/api/models.py
@@ -33,6 +33,14 @@ class Procedimento(models.Model):
 
   def __str__(self):
     return self.nome
+  
+class Insumo(models.Model):
+  nome = models.CharField(max_length=255)
+  valor = models.DecimalField(max_digits=10, decimal_places=2)
+  descricao = models.TextField(blank=True, null=True)
+
+  def __str__(self):
+    return self.nome
 
 class OrdemServico(models.Model):
   cliente = models.ForeignKey("Cliente", on_delete=models.CASCADE, related_name='ordens_cliente')
