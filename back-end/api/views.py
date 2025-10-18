04870f5b8ef3dab3960f1220d5472a22b55015b9
ariadny <ariadnyfran15@gmail.com>
2025-10-17 22:10:09 -0300

    feat: add metodo post de insumos

--- back-end/api/views.py
+++ back-end/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Cliente, Veiculo, Procedimento, OrdemServico
-from .serializers import ClienteSerializer, VeiculoSerializer, ProcedimentoSerializer, OrdemServicoSerializer
+from .models import Cliente, Veiculo, Procedimento, OrdemServico, Insumo
+from .serializers import ClienteSerializer, VeiculoSerializer, ProcedimentoSerializer, OrdemServicoSerializer, InsumoSerializer
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
@@ -178,3 +178,12 @@ def delete_ordem(request, pk):
   ordem.delete()
   
   return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['POST'])
+def create_insumo(request):  
+  if request.method == "POST":
+    serializer = InsumoSerializer(data=request.data) 
+    if serializer.is_valid(): # valida os dados
+      serializer.save() # salva os dados no banco
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
