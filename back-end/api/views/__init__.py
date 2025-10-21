4c23c974a6913b5fa643293bfa3a1975a5c7be44
Riam Stefesom <riamstefeson616@gmail.com>
2025-10-21 10:06:31 -0300

    refactor: colocando o novo arquivo de view no __init__.

--- back-end/api/views/__init__.py
+++ back-end/api/views/__init__.py
@@ -2,3 +2,4 @@ from .cliente_view import ClienteViewSet
 from .veiculo_view import VeiculoViewSet
 from .procedimento_view import ProcedimentoViewSet
 from .ordem_de_servico_view import OrdemServicoViewSet
+from .insumo_view import InsumoViewSet
