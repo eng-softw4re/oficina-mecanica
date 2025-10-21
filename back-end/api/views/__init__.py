759b649f622ee5337d13aa69390445a15c711eb7
Riam Stefesom <riamstefeson616@gmail.com>
2025-10-21 13:29:25 -0300

    refactor: adicionando a view insumos_ordem_servico no arquivo __init__

--- back-end/api/views/__init__.py
+++ back-end/api/views/__init__.py
@@ -3,3 +3,4 @@ from .veiculo_view import VeiculoViewSet
 from .procedimento_view import ProcedimentoViewSet
 from .ordem_de_servico_view import OrdemServicoViewSet
 from .insumo_view import InsumoViewSet
+from .insumo_ordem_servico_view import InsumoOrdemServicoViewSet
