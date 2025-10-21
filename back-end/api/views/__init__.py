e3193851b8827afed344f65cba5a3cfb5fea9ca3
Riam Stefesom <riamstefeson616@gmail.com>
2025-10-21 09:46:55 -0300

    feat: adicionando a view de ordem de serviço em __init__.

--- back-end/api/views/__init__.py
+++ back-end/api/views/__init__.py
@@ -1,3 +1,4 @@
 from .cliente_view import ClienteViewSet
 from .veiculo_view import VeiculoViewSet
 from .procedimento_view import ProcedimentoViewSet
+from .ordem_de_servico_view import OrdemServicoViewSet
