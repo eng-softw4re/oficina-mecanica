67ce1e3ade5af868cae2d30e4df02cf4dff6abef
ariadny <ariadnyfran15@gmail.com>
2025-10-22 10:37:06 -0300

    refactor: ajustando alinhamento

--- back-end/api/views/cobranca_view.py
+++ back-end/api/views/cobranca_view.py
@@ -8,23 +8,21 @@ from ..models import Cobranca
 from ..serializers import CobrancaSerializer
 
 class CobrancaViewSet(viewsets.ModelViewSet):
-  queryset = Cobranca.objects.all()
-  serializer_class = CobrancaSerializer
+    queryset = Cobranca.objects.all()
+    serializer_class = CobrancaSerializer
 
-  @action(detail=False, methods=['delete'], url_path='delete')
-  def delete_by_id(self, request):
-    id = request.data.get("id")
+    @action(detail=False, methods=['delete'], url_path='delete')
+    def delete_by_id(self, request):
+        id = request.data.get("id")
 
-    if not id:
-      return Response(
-        # Mensagem atualizada para "cobrança"
-        { "details": "Informe um ID válido para deletar a cobrança." },
-        status=status.HTTP_400_BAD_REQUEST
-      )
-    
-    # Busca o objeto Cobranca ou retorna 404
-    cobranca = get_object_or_404(Cobranca, id=id)
-    cobranca.delete()
-    
-    # Retorna 204 No Content, que é o padrão para DELETE bem-sucedido
-    return Response(status=status.HTTP_204_NO_CONTENT)
+        if not id:
+            return Response(
+            # Mensagem atualizada para "cobrança"
+                { "details": "Informe um ID válido para deletar a cobrança." },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        # Busca o objeto Cobranca ou retorna 404
+        cobranca = get_object_or_404(Cobranca, id=id)
+        cobranca.delete()
+        # Retorna 204 No Content, que é o padrão para DELETE bem-sucedido
+        return Response(status=status.HTTP_204_NO_CONTENT)
