362f89133f38ae3eae14c1a7ee6433c4d61c3435
Riam Stefesom <riamstefeson616@gmail.com>
2025-10-23 09:30:23 -0300

    refactor: criando a url para Endereço

--- back-end/api/urls.py
+++ back-end/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    ClienteViewSet, VeiculoViewSet, ProcedimentoViewSet, OrdemServicoViewSet, InsumoViewSet, InsumoOrdemServicoViewSet, CobrancaViewSet
+    ClienteViewSet, VeiculoViewSet, ProcedimentoViewSet, OrdemServicoViewSet, InsumoViewSet, InsumoOrdemServicoViewSet, CobrancaViewSet,
+    EnderecoViewSet, 
 )
 
 router = DefaultRouter()
@@ -13,6 +14,7 @@ router.register(r'ordem-de-servicos', OrdemServicoViewSet, basename='ordem-de-se
 router.register(r'insumos', InsumoViewSet, basename='insumo')
 router.register(r'insumos-ordem-de-servicos', InsumoOrdemServicoViewSet, basename='insumos-ordem-de-servico')
 router.register(r'cobranca', CobrancaViewSet, basename='cobranca')
+router.register(r'enderecos', EnderecoViewSet, basename='endereco')
 
 urlpatterns = [
     path('', include(router.urls)),
