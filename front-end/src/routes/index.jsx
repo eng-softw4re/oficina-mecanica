0749019cad56efae53a4e3947adce1c88cfb3c08
Riam Stefesom <riamstefeson616@gmail.com>
2025-10-24 09:13:06 -0300

    feat: adicionando url para HomePage, dentro das rotas protegidas.

--- front-end/src/routes/index.jsx
+++ front-end/src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import ProtectedRoutes from './ProtectedRoutes';
+import HomePage from '../pages/HomePage';
 
 
 const router = createBrowserRouter([
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
     path:  '/',
     element: <ProtectedRoutes />,
     children: [ // rotas acessíveis para usuários logados.
+      {
+        path: '/home',
+        element: <HomePage />
+      }
     ]
   }
 ])
