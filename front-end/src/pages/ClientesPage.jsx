88879dc2d0f045d5277bff1c1d6df4b7a3467992
Riam Stefesom <riamstefeson616@gmail.com>
2025-10-31 16:16:18 -0300

    refactor: fazendo mudanças no css

--- front-end/src/pages/ClientesPage.jsx
+++ front-end/src/pages/ClientesPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import clienteService from '../services/clienteServices';
 import { Link } from 'react-router-dom';
+import styles from './ClientesPage.module.css'
 
 function ClientesPage() {
   const [clientes, setClientes] = useState([]);
@@ -44,33 +45,32 @@ function ClientesPage() {
   }
 
   return (
-    <div className="container mx-auto p-4">
+    <div className={styles.container}>
       <h1 className="text-3xl font-bold mb-4">Lista de Clientes</h1>
       
       {/* TODO: Adicionar um botão de "Novo Cliente" aqui */}
 
-      <div className="bg-white shadow-md rounded-lg overflow-hidden">
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
+      <div className={styles.sub_container}>
+        <table>
+          <thead>
             <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nome</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">CPF</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Telefone</th>
-              <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">Ações</th>
+              <th>Nome</th>
+              <th>CPF</th>
+              <th>Telefone</th>
+              <th>Ações</th>
             </tr>
           </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
+          <tbody>
             {clientes.map((cliente) => (
               // Lembre-se da 'key' única para cada item da lista
               <tr key={cliente.id}> 
-                <td className="px-6 py-4 font-medium text-gray-900">{cliente.nome}</td>
-                <td className="px-6 py-4 text-gray-700">{cliente.cpf}</td>
-                <td className="px-6 py-4 text-gray-700">{cliente.telefone}</td>
-                <td className="px-6 py-4 text-right">
+                <td>{cliente.nome}</td>
+                <td>{cliente.cpf}</td>
+                <td>{cliente.telefone}</td>
+                <td>
                   {/* O Link do React Router para a página de detalhes */}
                   <Link 
                     to={`/clientes/${cliente.id}`}
-                    className="text-blue-600 hover:text-blue-900"
                   >
                     Ver Detalhes
                   </Link>
